508f2568f26a9ce80bf7796ea4bbb5746e7188e0
lucas <lucas.raggi@hotmail.com>
2019-03-13 22:39:05 -0300

    pre-process method with regex

--- java_line_remover/method_line_remover.py
+++ java_line_remover/method_line_remover.py
@@ -3,17 +3,25 @@ import re
 import pandas as pd
 
 
-def separate_valid_lines(method):
-    lines = method.split('\n')
-    for line in lines:
-        print(line)
+def pre_process_method(lines):
+    lines = lines.split('\n')
+    lines = filter(lambda x: not re.match(r'^\s*$', x), lines)  # remove whitespaces
+    lines = filter(lambda x: not re.match(r'^[\t]*[ ]*//', x), lines)  # remove lines that only have comments
+    lines = list(lines)
+    for i in range(len(lines)):
+        lines[i] = re.sub(r'[ ]*//.*', '', lines[i])
+    return lines
 
+
+def separate_removable_lines(method_lines):
+    pass
 
 def main():
     methods = pd.read_csv('result.csv')
     for index, row in methods.iterrows():
-        # print(row['codes'])
-        separate_valid_lines(row['codes'])
+        method = row['codes']
+        method_lines = pre_process_method(method)
+        separate_removable_lines(method_lines)
         break
 
 
