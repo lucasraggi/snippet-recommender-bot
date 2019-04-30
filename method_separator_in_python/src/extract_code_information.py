54bcabffdb870b647a562d56aa2a9400f3574605
lucasraggi89 <lrr@ic.ufal.br>
2019-04-30 10:36:17 -0300

    extractor isolating parameters

--- method_separator_in_python/src/extract_code_information.py
+++ method_separator_in_python/src/extract_code_information.py
@@ -1,6 +1,21 @@
 # extract number of parameters, type of parameters, type of return of method
 
 
+def get_parameters(code_header):
+    begin = -1
+    end = -1
+    for i in range(0, len(code_header)):
+        if code_header[i] == '(':
+            begin = i
+    for i in reversed(range(0, len(code_header))):
+        if code_header[i] == ')':
+            end = i
+    if begin == -1 or end == -1:
+        parameter = None
+    else:
+        parameter = code_header[begin + 1:end]
+    return parameter
+
 
 
 def extractor():
