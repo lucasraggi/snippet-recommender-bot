c7c12826a19b1a93f0c505572e884b7c98292bb2
lucasraggi89 <lrr@ic.ufal.br>
2019-04-30 10:35:51 -0300

    extractor created

--- method_separator_in_python/src/extract_code_information.py
+++ method_separator_in_python/src/extract_code_information.py
@@ -1,12 +1,17 @@
-
-
 # extract number of parameters, type of parameters, type of return of method
 
-
-def get_number_parameters():
 
 
 
 def extractor():
-    code =
-    number_parameters = get_number_parameters():
+    file = open('in', 'r')
+    code_header = file.readline()
+    print(code_header, end='')
+    parameter = get_parameters(code_header)  # begin and end of parameters
+    if parameter is None:
+        return
+    number_parameters = get_number_parameters(parameter)
+    types_parameters = get_types_parameters(parameter)
+
+
+extractor()
