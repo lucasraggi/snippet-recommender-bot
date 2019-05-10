381a402942907c205b95562447d011f8f5359c1a
lucasraggi89 <lrr@ic.ufal.br>
2019-05-10 10:50:55 -0300

    rank method complete

--- code_recommender/src/recommender.py
+++ code_recommender/src/recommender.py
@@ -20,8 +20,32 @@ class RecommendationMethod:
 
 def rank_methods(user_method, recommendation_method_list):
     for method in recommendation_method_list:
-        points = 0
-        
+        # Return types points
+        return_type_points = 0
+        if user_method.return_type == method.return_type:
+            return_type_points = 1
+        # Number parameters points
+        if user_method.number_parameters >= method.number_parameters:
+            number_parameters_points = user_method.number_parameters / method.number_parameters
+        else:
+            number_parameters_points = method.number_parameters / user_method.number_parameters
+
+        # Parameter types points
+        if len(user_method.parameter_types) > len(method.parameter_types):
+            bigger = user_method
+            smaller = method.parameter_types
+        else:
+            bigger = method.parameter_types
+            smaller = user_method
+        can_match = []
+        match = 0
+        for i in bigger:
+            for j in smaller:
+                if i == j and can_match[j] is not False:
+                    match += 1
+                    can_match[j] = False
+        parameter_type_points = match/len(bigger) * 2
+        method.points = return_type_points + number_parameters_points + parameter_type_points
     return recommendation_method_list
 
 
@@ -34,5 +58,6 @@ def recommender(method_name, number_parameters, parameter_types, return_type):
         method = RecommendationMethod(similar_method, 0)
         recommendation_method_list.append(method)
     recommendation_method_list = rank_methods(user_method, recommendation_method_list)
+    recommendation_method_list.sort(key=lambda x: x.points, reverse=True)
 
 
