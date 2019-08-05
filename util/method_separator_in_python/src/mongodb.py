316d7436a8c651d22bbb48d68285037ee0838f3c
lucasraggi89 <lrr@ic.ufal.br>
2019-08-05 11:10:08 -0300

    mongodb merge collections add

--- util/method_separator_in_python/src/mongodb.py
+++ util/method_separator_in_python/src/mongodb.py
@@ -38,6 +38,9 @@ class MongoDb:
         ]
         pprint.pprint(list(self.collection.aggregate(pipeline)))
 
+    def merge_collections(self, collection_name_list, merged_collection_name):
+        for collection_name in collection_name_list:
+            self.db[collection_name].copyTo(merged_collection_name)
 
 
 # data = MongoDb('java3')
